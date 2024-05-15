bd7a01ebc7cc113b5f677aa3200a57b20ed90d75
ZsomborHajzer <zsombor.hajzer@student.nhlstenden.com>
2024-05-15 15:33:50 +0200

    server messages update

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
     message = JSON.parse(message)
+    console.log(message)
 
     //! When connecting, the client sends a message automatically with a username field set by the client.
     if (message.hasOwnProperty("username")) {
@@ -32,7 +33,25 @@ wss.on('connection', function connection(ws) {
         TODO -- Here implement a switch case system in which different message types can be sent between clients. 
         TODO -- The clients should denote the event type as their first parameter and accordingly different message formating functions can be build for each usecase
         */
-        clients[id].send(generalMessage(ws.id, ws.username, message));
+
+        switch (message.event) {
+          case "endGame":
+              clients[id].send(endGameMessage(ws.id, ws.username, message));
+              break;
+          case "endTurn":
+              clients[id].send(endTurnMessage(ws.id, ws.username, message));
+              break;
+          case "moveAction":
+            clients[id].send(moveActionMessage(ws.id, ws.username, message));
+              break;
+          case "startGame":
+              clients[id].send(startGameMessage(ws.id, ws.username, message));
+              break;
+          default:
+            clients[id].send(generalMessage(ws.id, ws.username, message));
+              break;
+        }
+        return;
       }
     }
   });
@@ -82,4 +101,46 @@ function disconnectMessage(id, username) {
     id: id
   };
   return JSON.stringify(disconnectMessage)
+}
+
+function endGameMessage(id, username, message) {
+  const endGameMessage = {
+    event: message.event,
+    username: username,
+    id: id,
+    timeStamp: message.timeStamp
+  }
+  return JSON.stringify(endGameMessage)
+}
+
+function startGameMessage(id, username, message) {
+  const startGameMessage = {
+    event: message.event,
+    username: username,
+    id: id,
+    timeStamp: message.timeStamp
+  }
+  return JSON.stringify(startGameMessage)
+}
+
+function moveActionMessage(id, username, message) {
+  const moveActionMessage = {
+    event: message.event,
+    username: username,
+    id: id,
+    fromPosition: message.fromPosition,
+    toPosition: message.toPosition,
+    timeStamp: message.timeStamp
+  }
+  return JSON.stringify(moveActionMessage)
+}
+
+function endTurnMessage(id, username, message) {
+  const endTurnMessage = {
+    event: message.event,
+    username: username,
+    id: id,
+    timeStamp: message.timeStamp
+  }
+  return JSON.stringify(endTurnMessage)
 }
